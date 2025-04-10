1e09db103b0267392d5c878478e78586ac802302
saito.k <saito.k.ch@m.titech.ac.jp>
2025-04-10 18:26:17 +0900

    modified scripts for qsub

--- scripts/tsubame/qsub_all_node_q.sh
+++ scripts/tsubame/qsub_all_node_q.sh
@@ -1,19 +1,17 @@
-# Your env vars
-REPO_PATH="/gs/fs/tga-okazaki/saito/lighteval_swallow_dev/swallow-evaluation-instruct-private"
-HUGGINGFACE_CACHE="/gs/bs/tga-okazaki/saito/HF_HOME"
-
-# Model-dependent vars
+# Model-dependent settings
 MODEL_NAME="tokyotech-llm/Llama-3.1-Swallow-8B-Instruct-v0.3"
 SYSTEM_MESSAGE="あなたは誠実で優秀な日本人のアシスタントです。"
 MAX_CONTEXT_WINDOW=8192
 
-# Other paths
-RESULTS_PATH="${REPO_PATH}/results/${MODEL_NAME}"
-SCRIPTS_PATH="${REPO_PATH}/scripts/tsubame"
+# Other settings
+source "$(dirname "$0")/../user_config.sh"
+RESULTS_PATH="${REPO_PATH}/results/${MODEL_NAME}"; SCRIPTS_PATH="${REPO_PATH}/scripts/tsubame"
+QSUB_CMD="qsub -g tga-okazaki -l node_q=1"
 
+###########
 
 # Japanese
 # JMMLU
 JMMLU_RESULT_PATH="${RESULTS_PATH}/ja/jmmlu"
 mkdir -p "${JMMLU_RESULT_PATH}"
-qsub -g tga-okazaki -o "${JMMLU_RESULT_PATH}/logs/" -e "${JMMLU_RESULT_PATH}/logs/" -l node_q=1 -l h_rt=01:00:00 "${SCRIPTS_PATH}/evaluate_ja_jmmlu.sh" "${REPO_PATH}" "${HUGGINGFACE_CACHE}" "${JMMLU_RESULT_PATH}" "${MODEL_NAME}" "${SYSTEM_MESSAGE}" "${MAX_CONTEXT_WINDOW}"
+$QSUB_CMD -l h_rt=01:00:00 -o "${JMMLU_RESULT_PATH}/logs/" -e "${JMMLU_RESULT_PATH}/logs/" "${SCRIPTS_PATH}/evaluate_ja_jmmlu.sh" "${REPO_PATH}" "${HUGGINGFACE_CACHE}" "${JMMLU_RESULT_PATH}" "${MODEL_NAME}" "${SYSTEM_MESSAGE}" "${MAX_CONTEXT_WINDOW}"
---
+++ scripts/user_config.sh
@@ -0,0 +1,7 @@
+# Paths
+REPO_PATH="/path/to/your/path"
+HUGGINGFACE_CACHE="/path/to/your/hugginface_cache"
+
+# APIs
+OPENAI_API_KEY=sk-iloveswallow
+HF_TOKEN=hf_iloveswallow
